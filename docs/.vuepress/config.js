925b3e43a0dbcd788bdc5d1ddbb16cb07e0aa83a
coffee1900 <coffee1900@yeah.net>
2023-05-05 13:46:59 +0800

    fix：基础框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+// https://doc.houdunren.com/
+
+module.exports = {
+  title: "zdoc.title",
+  description: "zdoc.description",
+
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "前端路线", link: "/front/" },
+      { text: "工具软件", link: "/tool/" },
+      { text: "插件扩展", link: "/plugin/" },
+      { text: "视频教程", link: "https://www.bilibili.com/" },
+      { text: "GitHub", link: "https://github.com/coffee1900/zdoc/" },
+    ],
+  },
+};
